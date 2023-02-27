663f98b28d0213a2c1478ba9eb1ed9faf2684afe
Alethea Toh <aletheatohh@gmail.com>
2023-02-27 15:57:11 +0800

    Initial Commit

---
+++ src/main/java/RoomMicroservice/roomMicroservice/RoomMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package RoomMicroservice.roomMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RoomMicroservice/roomMicroservice/RoomMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package RoomMicroservice.roomMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
